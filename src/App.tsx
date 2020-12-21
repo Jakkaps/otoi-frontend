3f0635cc2c3a05a5fc2cdd7f8cf2e9d584fe0a40
Jens Kristoffersen <jensakrr@gmail.com>
2020-12-21 15:23:53 +0100

    Set up React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
-import React from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import blueGray from '@material-ui/core/colors/blueGrey'
+import React, { useState } from "react";
+import "./App.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import blueGray from "@material-ui/core/colors/blueGrey";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Projects from "./screens/Projects";
+import Services from "./screens/Services";
 
 export default function App() {
   const theme = createMuiTheme({
     palette: {
-      primary: blueGray
+      primary: blueGray,
     },
   });
 
+  const [selected, setSelected] = useState("Projects");
 
   return (
+    <Router>
       <ThemeProvider theme={theme}>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+        <AppBar position="static">
+          <Toolbar>
+            <img
+              src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png"
+              alt="logo"
+              className="Logo"
+            />
+            <Link
+              to="/projects"
+              className="AppBarLink"
+              id={selected === "Projects" ? "selected" : ""}
+              onClick={() => setSelected("Projects")}
+            >
+              Projects
+            </Link>
+            <Link
+              to="/services"
+              className="AppBarLink"
+              id={selected === "Services" ? "selected" : ""}
+              onClick={() => setSelected("Services")}
+            >
+              Services
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+        </Switch>
       </ThemeProvider>
+    </Router>
   );
 }
