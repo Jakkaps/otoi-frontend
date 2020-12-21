06c1f83943566a4ea74062790acf289bb572d3c8
Jens Kristoffersen <jensakrr@gmail.com>
2020-12-21 14:12:58 +0100

    Clean up files from create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import blueGray from '@material-ui/core/colors/blueGrey'
 
-function App() {
+export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: blueGray
+    },
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </ThemeProvider>
   );
 }
-
-export default App;
